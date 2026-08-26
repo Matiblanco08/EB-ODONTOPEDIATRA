4bf88efd02cd1cee448f34416e09aa27a20c8304
Matiblanco08 <matias.julio.blanco@gmail.com>
2026-08-26 20:15:17 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+/* Pegá acá la URL de tu implementación de Apps Script
+   (Implementar > Nueva implementación > Aplicación web > Copiar URL).
+   Termina en /exec */
+const CLINICA_API_URL = "https://script.google.com/macros/s/AKfycbw7wc0JlClyb_OBKhM0_2m6iTawDc7VAbVqZmHCo7O0rnpv_UAJhC-Ub13N0hS4MwcN/exec";
